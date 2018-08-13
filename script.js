1bdcdb931d950c8b210ce45bc3ff0d344fbbcf86
Jon McCarthy <jtmcc93@gmail.com>
2018-08-12 20:06:47 -0700

    add age function, create skills section, bio, text

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  function myAge(year, month, date) {
+    let d, n;
+    d = new Date();
+    n = d.getFullYear() - year;
+
+    if (d.getMonth() <= month && d.getDate() < date) {
+      return n--;
+    } else {
+      return n;
+    }
+  }
+
+  document.getElementById("age").innerHTML = myAge(1993, 4, 19);
+});
